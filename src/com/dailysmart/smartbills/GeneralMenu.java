29fc50719c8b0563af45a760113c1a6cf807a818
Alex <alexsikombe@gmail.com>
2014-01-03 16:26:05 +0100

    Ajout d'une vue

---
+++ src/com/dailysmart/smartbills/GeneralMenu.java
@@ -0,0 +1,14 @@
+package com.dailysmart.smartbills;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class GeneralMenu extends Activity 
+{
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+	}
+}
